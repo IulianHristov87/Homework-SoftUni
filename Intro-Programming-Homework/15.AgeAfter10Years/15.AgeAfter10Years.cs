1aad965fc778dd1dceaa745f20367f7290540251
IulianHristov87 <iulianhristov87@abv.bg>
2015-08-26 15:10:52 +0300

    Intro-Programming-Homework
    New

---
+++ Intro-Programming-Homework/15.AgeAfter10Years/15.AgeAfter10Years.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _15.AgeAfter10Years
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your birthday");
+        DateTime birthday = DateTime.Parse(Console.ReadLine());
+        DateTime today = DateTime.Now;
+        int age = today.Year - birthday.Year;
+        if ((birthday.Month <= today.Month && birthday.Day <= today.Day) ^ (birthday.Month < today.Month && birthday.Day > today.Day))
+        {
+            Console.WriteLine("You are {0} years old.", age);
+            Console.WriteLine("After 10 years you will be {0} years old", age + 10);
+        }
+        else
+        {
+            Console.WriteLine("You are {0} years old.", age - 1);
+            Console.WriteLine("After 10 years you will be {0} years old", age + 9);
+        }
+        
+    }
+        }
+    }
+
---
+++ Intro-Programming-Homework/15.CurrentDateAndTime/14.CurrentDateAndTime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _14.CurrentDateAndTime
+{
+    class CurrentDateAndTime
+    {
+        static void Main()
+        {
+            DateTime localDateAndTime = DateTime.Now;
+            Console.WriteLine("Current date and time are : " + localDateAndTime + " ч.");
+        }
+    }
+}
