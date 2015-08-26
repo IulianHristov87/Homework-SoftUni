b231af7d49f356947830a56b61cb24bdab883780
IulianHristov87 <iulianhristov87@abv.bg>
2015-08-26 16:31:42 +0300

    Into-Programming-Homework
    Correction

--- Intro-Programming-Homework/09.PrintASequenceVariant1/Properties/AssemblyInfo.cs
+++ Intro-Programming-Homework/09.PrintASequenceVariant1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PrintASequence")]
+[assembly: AssemblyTitle("09.PrintASequenceVariant1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PrintASequence")]
+[assembly: AssemblyProduct("09.PrintASequenceVariant1")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("de3b1420-b93f-49b8-a45a-457be481f4d5")]
+[assembly: Guid("9dd433aa-13bd-4dc2-85ae-4919acd47fed")]
 
 // Version information for an assembly consists of the following four values:
 //
