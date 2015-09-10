07933b78ff6dfee72e3e7450e373bf245f31d66d
IulianHristov87 <iulianhristov87@abv.bg>
2015-09-10 22:38:03 +0300

    New
    New homewrk

--- ConsoleInputOutputHomework/04.Number Comparer/04.NumberComparer.cs
+++ ConsoleInputOutputHomework/04.Number Comparer/04.NumberComparer.cs
@@ -17,26 +17,8 @@ namespace _04.Number_Comparer
             double a = double.Parse(Console.ReadLine());
             Console.WriteLine("Please enter number b");
             double b = double.Parse(Console.ReadLine());
-            string greaterTxt = "greater";
-            char aTxt = 'a';
-            char bTxt = 'b';
-
-            if (a > b)
-            {
-                double greater = a;
-                Console.WriteLine("|{0,-10}|{1,-10}|{2,-10}|", aTxt, bTxt, greaterTxt);
-                Console.WriteLine("|{0,-10}|{1,-10:F2}|{2,-10:F2}|", a, b, greater);
-            }
-            else if (b > a)
-            {
-                double greater = b;
-                Console.WriteLine("|{0,-10}|{1,-10}|{2,-10}|", aTxt, bTxt, greaterTxt);
-                Console.WriteLine("|{0,-10}|{1,-10}|{2,-10}|", a, b, greater);
-            }
-            else
-            {
-                Console.WriteLine("Numbers are equal");
-            }
+            Console.WriteLine();
+            Console.WriteLine("Biggest number is: " + Math.Max(a, b));
         }
     }
 }
