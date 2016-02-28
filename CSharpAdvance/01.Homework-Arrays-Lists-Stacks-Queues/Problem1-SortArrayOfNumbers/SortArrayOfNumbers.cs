9b47c487a2cdf17be4ece8159d663695f9910cd0
IulianHristov87 <iulianhristov87@abv.bg>
2016-02-28 09:08:44 +0200

    new
    new

---
+++ CSharpAdvance/01.Homework-Arrays-Lists-Stacks-Queues/Problem1-SortArrayOfNumbers/SortArrayOfNumbers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Problem1_SortArrayOfNumbers
+{
+    class SortArrayOfNumbers
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string[] inputClear = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] numbersForSort = Array.ConvertAll(inputClear, element => int.Parse(element));
+            int arrayLenght = numbersForSort.Length;
+
+            for (int x = 0; x < arrayLenght - 1; x++)
+            {
+                int exchangeNumber;
+                while (numbersForSort[x] > numbersForSort[x+1])
+                {
+                    
+                    exchangeNumber = numbersForSort[x+1];
+                    numbersForSort[x+1] = numbersForSort[x];
+                    numbersForSort[x] = exchangeNumber;
+                }
+                for (int y = 0; y < arrayLenght - 1; y++)
+                {
+                    
+                    while (numbersForSort[y] > numbersForSort[y + 1])
+                    {
+
+                        exchangeNumber = numbersForSort[y + 1];
+                        numbersForSort[y + 1] = numbersForSort[y];
+                        numbersForSort[y] = exchangeNumber;
+                    }
+                }
+            }
+           
+            for (int i = 0; i < numbersForSort.Length; i++)
+            {
+                Console.Write(numbersForSort[i]+" ");
+            }
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ CSharpAdvance/01.Homework-Arrays-Lists-Stacks-Queues/Problem2.SortArrayUsingSelectionSort/SortArrayOfNumbersUsingSelectionSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Problem2.SortArrayUsingSelectionSort
+{
+    class SortArrayOfNumbersUsingSelectionSort
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string[] inputClear = input.Split(new [] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            int[] numbersForSort = Array.ConvertAll(inputClear,int.Parse);
+            int arrayLenght = numbersForSort.Length;
+
+            int[] result = new int[arrayLenght];
+
+            
+
+            for (int x = 0; x < arrayLenght - 1; x++)
+            {
+                int minNumber=0;
+                int count = 0;
+                if (numbersForSort[x] < minNumber)
+                {
+                
+                }
+                
+            }
+
+            for (int i = 0; i < inputClear.Length; i++)
+            {
+                Console.Write(result[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+        
+    }
+}
